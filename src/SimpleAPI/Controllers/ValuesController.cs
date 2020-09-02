263ce32fec98c1daad89c378feac31a2664dc27e
emilyduansit <Emily.duan.co@gmail.com>
2020-09-02 21:56:53 +1200

    update value/id

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -24,6 +24,11 @@ namespace SimpleAPI.Controllers
            switch(id)
            {
                case 1: return "Monday";
+               case 2: return "Tuesday";
+               case 3: return "Wednesday";
+               case 4: return "Thursday";
+               case 5: return "Friday";
+               case 6: return "Saturday";
                default: return "Sunday";
            }
         }
